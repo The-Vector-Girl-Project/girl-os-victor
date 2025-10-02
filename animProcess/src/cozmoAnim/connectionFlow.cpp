b1585fff51c27d1cc82e27c69dc7eb5cf983f434
MetalDEV <86743223+IAmDazen@users.noreply.github.com>
2025-10-02 19:41:57 +0100

    Update connectionFlow.cpp

--- animProcess/src/cozmoAnim/connectionFlow.cpp
+++ animProcess/src/cozmoAnim/connectionFlow.cpp
@@ -45,7 +45,7 @@ u32 _pin = 123456;
 
 const f32 kRobotNameScale = 0.7f;
 const std::string kURL = "v.pvic.xyz";
-const ColorRGBA   kColor(0.f, 0.804f, 1.f, 1.f);
+const ColorRGBA   kColor(1.f, 0.7137f, 0.7569f, 1.f);
 
 const char* kShowPinScreenSpriteName = "pairing_icon_key";
 
